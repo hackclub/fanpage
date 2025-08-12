4a028e2a935fc5940008a4bc9198a771c90e23bc
Owlfx <nirvaan.kohli@gmail.com>
2025-08-12 11:30:34 -0400

    Sep 1st, also getting rid of js/dist

--- fanpage/src/components/brand/Background.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-export default function Background() {
-    return (_jsxs("div", { "aria-hidden": true, className: "absolute inset-0 -z-10 isolate", children: [_jsx("div", { className: "bg-photo-img absolute inset-0 z-0" }), _jsx("div", { className: "bg-tint absolute inset-0 z-10" }), _jsx("div", { className: "grain absolute inset-0 z-20 pointer-events-none" }), _jsx("div", { className: "grain-2 absolute inset-0 z-20 pointer-events-none" }), _jsx("div", { className: "vignette absolute inset-0 z-30 pointer-events-none" })] }));
-}
